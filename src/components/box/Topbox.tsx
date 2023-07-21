ea3bf336b8deffaa430bc9777e022e33ff8a84c3
uzair1761 <ozairkhanan@gmail.com>
2023-07-22 01:22:24 +0500

    new changes

---
+++ src/components/box/Topbox.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Topbox = () => {
+  return (
+    <div  className="Top">
+      <h1>Top deal</h1>
+    </div>
+  )
+}
+
+export default Topbox
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,18 @@
 
 import './Home.scss'
+import Topbox from '../../components/box/Topbox'
 const Home = () => {
   return (
     <div className='home'>
-     home
+       <div className="box box1"><Topbox/></div>
+       <div className="box box2">box2</div>
+       <div className="box box3">box3</div>
+       <div className="box box4">box4</div>
+       <div className="box box5">box5</div>
+       <div className="box box6">box6</div>
+       <div className="box box7">box7</div>
+       <div className="box box8">box8</div> 
+        <div className="box box9">box9</div>
     </div>
   )
 }
