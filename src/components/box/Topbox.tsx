908455324a97abc24087babbc85d3d05b13959ad
uzair1761 <ozairkhanan@gmail.com>
2023-07-23 22:33:45 +0500

    new changes

--- src/components/box/Topbox.tsx
+++ src/components/box/Topbox.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import "./Topbox.scss"
+import { topDealUsers } from '../../data';
 
 const Topbox = () => {
   return (
-    <div  className="Top">
+    <div className="Top">
       <h1>Top deal</h1>
+      <div className="list">
+        {topDealUsers.map((user) => {
+          return (
+            <div key={user.id} className="listItem">
+              <div className="user">
+                <img src={user.img} alt={`User ${user.id}`} />
+                <div className="userText">
+            
+                    <span  className="username">{user.username}</span>
+    
+                <span className="userEmail"> 
+                    {user.email}
+                </span>
+               
+                </div>
+                <div>
+                  <span className='amount'>{user.amount}</span>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Topbox
+export default Topbox;
