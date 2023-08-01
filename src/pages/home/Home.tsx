c52269b14cd4c69aa23b6ee62202df9e20b95d45
uzair1761 <ozairkhanan@gmail.com>
2023-08-01 23:09:06 +0500

    new chnages

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,18 @@
 
 import './Home.scss'
 import Topbox from '../../components/box/Topbox'
+import ChartBox from '../../components/ChartBox/ChartBox'
 const Home = () => {
   return (
     <div className='home'>
        <div className="box box1"><Topbox/></div>
-       <div className="box box2">box2</div>
-       <div className="box box3">box3</div>
-       <div className="box box4">box4</div>
-       <div className="box box5">box5</div>
-       <div className="box box6">box6</div>
-       <div className="box box7">box7</div>
-       <div className="box box8">box8</div> 
+       <div className="box box2"><ChartBox/></div>
+       <div className="box box3"><ChartBox/></div>
+       <div className="box box4"></div>
+       <div className="box box5"><ChartBox/></div>
+       <div className="box box6"><ChartBox/></div>
+       <div className="box box7"></div>
+       <div className="box box8"></div> 
         <div className="box box9">box9</div>
     </div>
   )
