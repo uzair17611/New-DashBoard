4697c67a1dcb69dba0849ea1b7ea94ae62098e19
uzair1761 <ozairkhanan@gmail.com>
2023-08-19 00:24:44 +0500

    new changes

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -1,68 +1,40 @@
 import './DataTable.scss'
 // import Box from '@mui/material/Box';
 import { DataGrid, GridColDef, GridValueGetterParams,GridToolbar } from '@mui/x-data-grid';
+import { userRows } from '../../data';
 
-const DataTable = () => {
-    const columns: GridColDef[] = [
-        { field: 'id', headerName: 'ID', width: 90 },
-        {field :"avatar " ,headerName:"Avatar" , width:100,
-        renderCell :(params)=>{
-            return <img src={params.row.img || "/noavatar.png"}></img>
-        }
-    },
+type Props ={
+    columns:GridColDef,
+    row:object[]
+}
 
-        {
-          field: 'firstName',
-          headerName: 'First name',
-          width: 150,
-          editable: true,
-        },
-        {
-          field: 'lastName',
-          headerName: 'Last name',
-          width: 150,
-          editable: true,
-        },
-        {
-          field: 'age',
-          headerName: 'Age',
-          type: 'number',
-          width: 110,
-          editable: true,
-        },
-        {
-          field: 'fullName',
-          headerName: 'Full name',
-          description: 'This column has a value getter and is not sortable.',
-          sortable: false,
-          width: 160,
-          valueGetter: (params: GridValueGetterParams) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-        },
-      ];
-      
-      const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-      ];
+
+
+
+const DataTable = (props:Props) => {
+    
+    //   const rows = [
+    //     { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 ,status:true },
+    //     { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 ,status:true  },
+    //     { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    //     { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    //     { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null ,status:true  },
+    //     { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    //     { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 ,status:true },
+    //     { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    //     { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    //   ];
       
   return (
     <div className='DataTable'>
         <DataGrid
         className='Datagrid'
-        rows={rows}
-        columns={columns}
+        rows={props.row}
+        columns={props.columns}
         initialState={{
           pagination: {
             paginationModel: {
-              pageSize: 5,
+              pageSize: 7,
             },
           },
         }}
--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,6 +1,70 @@
 import "./user.scss"
 import DataTable from "../../components/DataTable/DataTable"
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { userRows } from "../../data";
+
+
+
+
+
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {field :"avatar " ,headerName:"Avatar" , width:100,
+  renderCell :(params)=>{
+      return <img src={params.row.img || "/noavatar.png"}></img>
+  }
+},
+{field :"Action" ,headerName:"Action" , width:100,
+renderCell :(params)=>{
+  return <div className="action">
+      <div className="view">View</div>
+      <div className="delete">Delete</div>
+  </div>
+}
+},
+{
+  field:"status" ,headerName:"status" , width:100 ,type :"boolean"
+},
+
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    editable: true,
+  },
+  
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params: GridValueGetterParams) =>
+      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  },
+];
+
+
+
 const User = () => {
+
+
+  
+  
   return (
     <div className='user'>
      <div className="info">
@@ -8,7 +72,7 @@ const User = () => {
       <button>
         Add New User
       </button>
-     <DataTable/>
+     <DataTable  columns={columns}  row={userRows}/>
 
      </div>
     </div>
